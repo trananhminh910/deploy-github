f362cd4e52bb8b9e98cf0d28121308717ae2cf7b
Tran Anh Minh <trananhminh910@gmail.com>
2020-06-29 22:41:37 +0700

    Updates

---
+++ precache-manifest.013e2ea4c73ee1815c4179ce07df9555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f290a87684140fbf6fb6856883940f",
+    "url": "/deploy-github/index.html"
+  },
+  {
+    "revision": "0435feb6ee0fcb599534",
+    "url": "/deploy-github/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9610221d70749b902cfd",
+    "url": "/deploy-github/static/js/2.00fa8dd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-github/static/js/2.00fa8dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0435feb6ee0fcb599534",
+    "url": "/deploy-github/static/js/main.cf6f5a0a.chunk.js"
+  },
+  {
+    "revision": "39317f873cd363a8eaa8",
+    "url": "/deploy-github/static/js/runtime-main.955354c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
